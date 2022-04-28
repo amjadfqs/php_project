9df2daa489b50c76bf04e954d041792f8d8cebcb
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2022-04-29 00:07:49 +0300

    First Commit

---
+++ pages/templates/header.php
@@ -0,0 +1,45 @@
+<head>
+    <link rel="stylesheet" href="../css/main.min.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    
+    
+</head>
+<body class="">
+  <!-- navbar -->
+  <nav class="shadow navbar navbar-expand-md navbar-light bg-primary mb-5">
+    <div class="container-xxl">
+      <!-- navbar brand / title -->
+      <a class="navbar-brand" href="index.php">
+        <span class="text-dark fw-bold">
+          <i class="bi bi-book-half"></i>
+          Name 
+        </span>
+      </a>
+      <!-- toggle button for mobile nav -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- navbar links -->
+      <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+        <ul class="navbar-nav text-center text-dark">
+          <li class="nav-item">
+            <a class="nav-link " href="#topics">HI</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#reviews">HI</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">HI</a>
+          </li>
+          <li class="nav-item d-md-none">
+            <a class="nav-link" href="#pricing">HI</a>
+          </li>
+          <li class="nav-item  ms-2 d-none d-md-inline">
+            <a class="btn btn-outline-dark rounded-pill" href="../add.php">HI</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
