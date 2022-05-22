15792a9368d1129dd8b1bb717092eaff06475a6a
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2022-05-22 12:11:39 +0300

    added profile btn

--- include/template/header.php
+++ include/template/header.php
@@ -37,6 +37,23 @@
       <div class="d-none d-md-block">
         <button class="btn btn-primary me-2" type="button">Login</button>
         <a class="btn btn-primary" role="button" href="#">Start a project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-4" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-list-4">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle">
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Dashboard</a>
+                <a class="dropdown-item" href="#">Edit Profile</a>
+                <a class="dropdown-item" href="#">Log Out</a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
       <!--end of botton for lg and md screen   -->
     </div>
