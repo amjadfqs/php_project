1b0614e03f772fcd5db01a7137561ad3a3ef15c2
Alia-mohammed <104635979+Alia-mohammed@users.noreply.github.com>
2022-05-23 21:00:04 +0300

    fixed the page

--- pages/projectDesc.php
+++ pages/projectDesc.php
@@ -1,6 +1,7 @@
 <?php
 $pageTitle = "Home Page";
-include './ini.php';
+include '../conf/ini.php';
+
 include $temp . 'header.php';
 
 ?>
@@ -63,34 +64,51 @@ include $temp . 'header.php';
                             Taizz,Yemen
                         </div>
                     </div> <br />
-                    <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</div>
 <br />
 <div class="container-fluid bg-secondary mt-5 p-3">
-    <div class="container">
+    <div class="container bg-light">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
+                    type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
+                    type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact"
+                    type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Update</button>
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                <p>
+                <h3>Story</h3>
+                Many fiber artists would love to make their own yarn, but traditional spinning wheels are expensive and
+                drop spindles can be slow or difficult to learn. This project offers an af.</p>
+
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                ....
+
+            </div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+
+
+
+
+
+
+            </div>
         </div>
     </div>
 </div>
-
 
 <?php include $temp . 'footer.php'; ?>
