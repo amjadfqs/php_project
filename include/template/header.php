48e6798768c2797d8623b2bd220fee09d098228e
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2022-05-27 09:52:11 +0300

    fixed modal 2

--- include/template/header.php
+++ include/template/header.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-if (isset($_SESSION['UserName'])) {
+if (isset($_SESSION['ID'])) {
     include $pages . 'popupProfile.php';
 }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,27 +19,20 @@ if (isset($_SESSION['UserName'])) {
     <nav id="navbar" class="navbar navbar-light navbar-expand-md border-3">
         <div class="container d-flex justify-content-between">
             <?php if (isset($_COOKIE['user'])) : ?>
-            <div class="dropdown d-md-none d-sm-flex justify-content-sm-end align-content-sm-end">
-                <a class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    <!-- <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle"> -->
-                    <i class="far fa-user-circle fa-2x"></i>
-                </a>
-                <ul class="dropdown-menu mt-1 text-center" aria-labelledby="dropdownMenuButton1">
-                    <li><a type="button" class="dropdown-item" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal1">Profile<i class="fas fa-user ms-3"></i></a></li>
-                    <li><a class="dropdown-item" href="<?= $pages ?>logout.php">Logout <i
-                                class="fas fa-sign-out-alt ms-2"></i></a></li>
-                </ul>
-            </div>
+                <div class="dropdown d-md-none d-sm-flex justify-content-sm-end align-content-sm-end">
+                    <a class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
+                        <!-- <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle"> -->
+                        <i class="far fa-user-circle fa-2x"></i>
+                    </a>
+                    <ul class="dropdown-menu mt-1 text-center" aria-labelledby="dropdownMenuButton1">
+                        <li><a type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal1">Profile<i class="fas fa-user ms-3"></i></a></li>
+                        <li><a class="dropdown-item" href="<?= $pages ?>logout.php">Logout <i class="fas fa-sign-out-alt ms-2"></i></a></li>
+                    </ul>
+                </div>
             <?php endif ?>
             <a class="navbar-brand d-flex align-items-center" href="<?= $pages; ?>index.php">
-                <span
-                    class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg
-                        xmlns="http://www.w3.org/2000/svg" width="25" height="50" viewBox="0 0 57 58">
-                        <path id="logo"
-                            d="M19.294,7.156a15,15,0,0,1,18.412,0l11.15,8.667A15,15,0,0,1,54,32.025L49.346,47.359A15,15,0,0,1,34.993,58H22.007A15,15,0,0,1,7.654,47.359L3,32.025a15,15,0,0,1,5.147-16.2Z"
-                            fill="#4dd5ac" />
+                <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="50" viewBox="0 0 57 58">
+                        <path id="logo" d="M19.294,7.156a15,15,0,0,1,18.412,0l11.15,8.667A15,15,0,0,1,54,32.025L49.346,47.359A15,15,0,0,1,34.993,58H22.007A15,15,0,0,1,7.654,47.359L3,32.025a15,15,0,0,1,5.147-16.2Z" fill="#4dd5ac" />
                     </svg>
                 </span>
                 HASH
@@ -62,11 +54,9 @@ if (isset($_SESSION['UserName'])) {
                 <!-- bottom for small screen -->
                 <div class="d-md-none my-2 text-center">
                     <?php if (isset($_COOKIE['user'])) : ?>
-                    <a id="SpecialBtn" class="d-none btn btn-primary me-2" type="button"
-                        href="<?= $pages; ?>login.php">Login</a>
+                        <a id="SpecialBtn" class="d-none btn btn-primary me-2" type="button" href="<?= $pages; ?>login.php">Login</a>
                     <?php else : ?>
-                    <a id="SpecialBtn" class="btn btn-primary me-2" type="button"
-                        href="<?= $pages; ?>login.php">Login</a>
+                        <a id="SpecialBtn" class="btn btn-primary me-2" type="button" href="<?= $pages; ?>login.php">Login</a>
                     <?php endif ?>
                     <a id="SpecialBtn" class="btn btn-primary" type="button">Start a project</a>
                 </div>
@@ -76,28 +66,23 @@ if (isset($_SESSION['UserName'])) {
 
             <div class="d-none d-md-block">
                 <?php if (isset($_COOKIE['user'])) : ?>
-                <a id="SpecialBtn" class="d-none btn btn-primary rounded-pill" type="button"
-                    href="<?= $pages; ?>login.php">Login</a>
+                    <a id="SpecialBtn" class="d-none btn btn-primary rounded-pill" type="button" href="<?= $pages; ?>login.php">Login</a>
                 <?php else : ?>
-                <a id="SpecialBtn" class="btn btn-primary rounded-pill" type="button"
-                    href="<?= $pages; ?>login.php">Login</a>
+                    <a id="SpecialBtn" class="btn btn-primary rounded-pill" type="button" href="<?= $pages; ?>login.php">Login</a>
                 <?php endif ?>
                 <a id="SpecialBtn" class="btn btn-primary rounded-pill" type="button" href="#">Start a project</a>
                 <!-- profile -->
                 <?php if (isset($_COOKIE['user'])) : ?>
-                <span class="dropdown">
-                    <a class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static"
-                        aria-expanded="false">
-                        <i class="far fa-user-circle fa-2x"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end mt-3 text-center">
-                        <li><a type="button" class="dropdown-item" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal1">Profile<i class="fas fa-user ms-3"></i></a></li>
+                    <span class="dropdown">
+                        <a class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                            <i class="far fa-user-circle fa-2x"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end mt-3 text-center">
+                            <li><a type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal1">Profile<i class="fas fa-user ms-3"></i></a></li>
 
-                        <li><a class="dropdown-item" href="<?= $pages ?>logout.php">Logout <i
-                                    class="fas fa-sign-out-alt ms-2"></i></a></li>
-                    </ul>
-                </span>
+                            <li><a class="dropdown-item" href="<?= $pages ?>logout.php">Logout <i class="fas fa-sign-out-alt ms-2"></i></a></li>
+                        </ul>
+                    </span>
                 <?php endif ?>
             </div>
             <!--end of bottom for lg and md screen   -->
--- pages/popupProfile.php
+++ pages/popupProfile.php
@@ -21,8 +21,7 @@ if (isset($_SESSION['ID'])) {
             </div>
             <div class="d-flex justify-content-center">
                 <a href="#">
-                    <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg"
-                        width="90" height="89" class="rounded-circle">
+                    <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="90" height="89" class="rounded-circle">
                 </a>
             </div>
             <div class="container">
