feea65e838c885ace997ecd079c50ee5c18368ca
adham <92870575+AdhamALmeklhafi@users.noreply.github.com>
2022-06-02 18:44:40 +0300

    Adding the session Admin and projects page

--- pages/projects.php
+++ pages/projects.php
@@ -4,172 +4,171 @@ include '../conf/ini.php';
 include $temp . 'header.php';
 ?>
 <div class="container">
-    <nav class="overflow-auto">
-        <ul class="d-flex justify-content-center mt-5">
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Art</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Comics</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Illustration</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Film</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Design</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Food</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Music</a>
-            </li>
-            <li class="nav-item line-before">
-                <a class="nav-link text-black" href="#">Games</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="d-flex justify-content-center mt-5">
+        <div class="btn-group">
+            <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Select Type 1
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Art</a></li>
+                <li><a class="dropdown-item" href="#">Comics</a></li>
+                <li><a class="dropdown-item" href="#">Illustration</a></li>
+                <li><a class="dropdown-item" href="#">Film</a></li>
+            </ul>
+        </div>
+        <div class="btn-group mx-3">
+            <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Select Type 2
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Design</a></li>
+                <li><a class="dropdown-item" href="#">Food</a></li>
+                <li><a class="dropdown-item" href="#">Music</a></li>
+                <li><a class="dropdown-item" href="#">Games</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
+<!-- starting Card -->
 <div class="container mt-5 overflow-auto">
     <div class="d-inline-flex">
-        <div class="card mx-2 my-4 shadow" style="width: 25rem;">
-            <a href="#"><img src="<?php echo $img; ?>card1.svg" class="card-img-top" alt=""></a>
+        <div class="card mx-2 my-4 shadow" style="width: 20rem;">
+            <a href="#"><img src="<?php echo $img; ?>card2.png" class="card-img-top" alt=""></a>
             <div class="card-body">
                 <h5 class="card-title text-primary">Donate To Educate</h5>
                 <p class="card-text">The FIFTH 200 PAGE HARDBACK BOOK
-                    of Lorenzo Etherington HOW TO THINK
+                    of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By
-                    Lorenzo Etherington of Lorenzo Etherington HOW TO THINK
+                    Lorenzo Sherrington of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By. </p>
                 <div class="card-text">
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Art & Design</i>
+                        <i class="fas fa-tags fa-flip-horizontal"></i>
+                        Art & Design
                     </div>
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Started at: 2021-05-20</i>
+                        <i class="fas fa-clock"></i>
+                        Started at: 2021-05-20
                     </div>
                     <div class="text-muted">
-                        <img class="me-2" src="<?php echo $img; ?>Card/location.svg">
-                        <i>Taizz,Yemen</i>
+                        <i class="fas fa-location-arrow"></i>
+                        Taizz,Yemen
                     </div>
                 </div> <br />
                 <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
             </div>
         </div>
-        <div class="card mx-2 my-4 shadow" style="width: 25rem;">
-            <a href="#"><img src="<?php echo $img; ?>card1.svg" class="card-img-top" alt=""></a>
+        <div class="card mx-2 my-4 shadow" style="width: 20rem;">
+            <a href="#"><img src="<?php echo $img; ?>card2.png" class="card-img-top" alt=""></a>
             <div class="card-body">
                 <h5 class="card-title text-primary">Donate To Educate</h5>
                 <p class="card-text">The FIFTH 200 PAGE HARDBACK BOOK
-                    of Lorenzo Etherington HOW TO THINK
+                    of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By
-                    Lorenzo Etherington of Lorenzo Etherington HOW TO THINK
+                    Lorenzo Sherrington of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By. </p>
                 <div class="card-text">
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Art & Design</i>
+                        <i class="fas fa-tags fa-flip-horizontal"></i>
+                        Art & Design
                     </div>
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Started at: 2021-05-20</i>
+                        <i class="fas fa-clock"></i>
+                        Started at: 2021-05-20
                     </div>
                     <div class="text-muted">
-                        <img class="me-2" src="<?php echo $img; ?>Card/location.svg">
-                        <i>Taizz,Yemen</i>
+                        <i class="fas fa-location-arrow"></i>
+                        Taizz,Yemen
                     </div>
                 </div> <br />
                 <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
             </div>
         </div>
-        <div class="card mx-2 my-4 shadow" style="width: 25rem;">
-            <a href="#"><img src="<?php echo $img; ?>card1.svg" class="card-img-top" alt=""></a>
+        <div class="card mx-2 my-4 shadow" style="width: 20rem;">
+            <a href="#"><img src="<?php echo $img; ?>card2.png" class="card-img-top" alt=""></a>
             <div class="card-body">
                 <h5 class="card-title text-primary">Donate To Educate</h5>
                 <p class="card-text">The FIFTH 200 PAGE HARDBACK BOOK
-                    of Lorenzo Etherington HOW TO THINK
+                    of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By
-                    Lorenzo Etherington of Lorenzo Etherington HOW TO THINK
+                    Lorenzo Sherrington of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By. </p>
                 <div class="card-text">
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Art & Design</i>
+                        <i class="fas fa-tags fa-flip-horizontal"></i>
+                        Art & Design
                     </div>
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Started at: 2021-05-20</i>
+                        <i class="fas fa-clock"></i>
+                        Started at: 2021-05-20
                     </div>
                     <div class="text-muted">
-                        <img class="me-2" src="<?php echo $img; ?>Card/location.svg">
-                        <i>Taizz,Yemen</i>
+                        <i class="fas fa-location-arrow"></i>
+                        Taizz,Yemen
                     </div>
                 </div> <br />
                 <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
             </div>
         </div>
-        <div class="card mx-2 my-4 shadow" style="width: 25rem;">
-            <a href="#"><img src="<?php echo $img; ?>card1.svg" class="card-img-top" alt=""></a>
+        <div class="card mx-2 my-4 shadow" style="width: 20rem;">
+            <a href="#"><img src="<?php echo $img; ?>card2.png" class="card-img-top" alt=""></a>
             <div class="card-body">
                 <h5 class="card-title text-primary">Donate To Educate</h5>
                 <p class="card-text">The FIFTH 200 PAGE HARDBACK BOOK
-                    of Lorenzo Etherington HOW TO THINK
+                    of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By
-                    Lorenzo Etherington of Lorenzo Etherington HOW TO THINK
+                    Lorenzo Sherrington of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By. </p>
                 <div class="card-text">
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Art & Design</i>
+                        <i class="fas fa-tags fa-flip-horizontal"></i>
+                        Art & Design
                     </div>
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Started at: 2021-05-20</i>
+                        <i class="fas fa-clock"></i>
+                        Started at: 2021-05-20
                     </div>
                     <div class="text-muted">
-                        <img class="me-2" src="<?php echo $img; ?>Card/location.svg">
-                        <i>Taizz,Yemen</i>
+                        <i class="fas fa-location-arrow"></i>
+                        Taizz,Yemen
                     </div>
                 </div> <br />
                 <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
             </div>
         </div>
-        <div class="card mx-2 my-4 shadow" style="width: 25rem;">
-            <a href="#"><img src="<?php echo $img; ?>card1.svg" class="card-img-top" alt=""></a>
+        <div class="card mx-2 my-4 shadow" style="width: 20rem;">
+            <a href="#"><img src="<?php echo $img; ?>card2.png" class="card-img-top" alt=""></a>
             <div class="card-body">
                 <h5 class="card-title text-primary">Donate To Educate</h5>
                 <p class="card-text">The FIFTH 200 PAGE HARDBACK BOOK
-                    of Lorenzo Etherington HOW TO THINK
+                    of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By
-                    Lorenzo Etherington of Lorenzo Etherington HOW TO THINK
+                    Lorenzo Sherrington of Lorenzo Sherrington HOW TO THINK
                     WHEN YOU DRAW series + reprinting
                     ALL original SOLD OUT BOOKS! By. </p>
                 <div class="card-text">
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Art & Design</i>
+                        <i class="fas fa-tags fa-flip-horizontal"></i>
+                        Art & Design
                     </div>
                     <div class="text-muted mb-2">
-                        <img class="me-2" src="<?php echo $img; ?>Card/start.svg">
-                        <i>Started at: 2021-05-20</i>
+                        <i class="fas fa-clock"></i>
+                        Started at: 2021-05-20
                     </div>
                     <div class="text-muted">
-                        <img class="me-2" src="<?php echo $img; ?>Card/location.svg">
-                        <i>Taizz,Yemen</i>
+                        <i class="fas fa-location-arrow"></i>
+                        Taizz,Yemen
                     </div>
                 </div> <br />
                 <span class="">By<a href="#" class="mx-1 text-primary">Adham Mustafa</a></span>
@@ -178,5 +177,4 @@ include $temp . 'header.php';
     </div>
 </div>
 <!-- end the card -->
-<hr class="text-black-50 m-0">
 <?php include $temp . 'footer.php'; ?>
