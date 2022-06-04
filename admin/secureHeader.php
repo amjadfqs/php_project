9d20b56b8f1ba675de17c92c27f87513ec6819ba
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2022-06-04 22:19:17 +0300

    Update secureHeader.php

--- admin/secureHeader.php
+++ admin/secureHeader.php
@@ -1,34 +1,5 @@
 <?php
 include $temp . 'js.php';
-// if (isset($_SESSION)) {
-//     if (!isset($_SESSION['GroupID'])) {
-//         echo '<div class="alert alert-danger text-center" role="alert">
-//                         Please, Login first!
-//                         </div>';
-//         echo "<div class='d-flex justify-content-center'>
-//             <img src=' $img/notfound.svg'>
-//         </div>";
-//         exit();
-//     } elseif ($_SESSION['GroupID'] != 1) {
-
-//         echo '<div class="alert alert-danger text-center" role="alert">
-//                     Unauthorized Access
-//                     </div>';
-//         echo "<div class='d-flex justify-content-center'>
-//             <img src=' $img/notfound.svg'>
-//         </div>";
-//         exit();
-//     }
-// } else {
-//     echo '<div class="alert alert-danger text-center" role="alert">
-//                         Please, Login first!
-//                         </div>';
-//     echo "<div class='d-flex justify-content-center'>
-//             <img src=' $img/notfound.svg'>
-//         </div>";
-//     exit();
-// }
-
 if (empty($_SESSION)) {
     echo '<div class="alert alert-danger text-center" role="alert">
                         Please, Login first!
